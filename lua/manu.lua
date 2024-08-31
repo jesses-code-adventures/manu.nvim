9ffc33cdf1347a3787344e68ba6f5074b828a5f1
jesse <jesse@rapidpaylegal.com>
2024-08-31 18:45:40 +1000

    update: start logic for request bounds

--- lua/manu.lua
+++ lua/manu.lua
@@ -4,6 +4,16 @@ local function current_win_buf()
     return vim.api.nvim_get_current_win(),vim.api.nvim_get_current_buf()
 end
 
+local function is_visual_mode()
+    local mode = vim.api.nvim_get_mode().mode
+    return mode == 'v' or mode == 'V' or mode == '^V'
+end
+
+local function get_start_pos_visual()
+  local _, start_row, start_col, _ = unpack(vim.fn.getpos("'<"))
+  return start_row, start_col
+end
+
 local function open_scratch_buffer()
   vim.cmd('rightbelow vsplit')
   local win_id = vim.api.nvim_get_current_win()
@@ -40,8 +50,35 @@ function M._set_request_postition()
     print("setting current pos to ", vim.inspect(cursor_pos))
 end
 
-function M.setup()
+function M._set_request_bounds(start_row, start_col, end_row, end_col)
+    M._req_start_row = start_row
+    M._req_start_col = start_col
+    M._req_end_row = end_row
+    M._req_end_col = end_col
+end
+
+function M._print_request_params()
+    local params = {
+        win = M._req_win_id,
+        buf = M._req_buf_id,
+        start_row = M._req_start_row,
+        start_col = M._req_start_col,
+        end_row = M._req_end_row,
+        end_col = M._req_end_col,
+    }
+    print(vim.inspect(params))
+end
+
+function M._init()
     M._set_request_win_buf()
+    local pos = vim.api.nvim_win_get_cursor(M._req_win_id)
+    M._set_request_bounds(1, 0, pos[0], pos[1])
+    print("initialized with request params: ")
+    M._print_request_params()
+end
+
+function M.setup()
+    M._init()
 end
 
 function M.open_chat()
